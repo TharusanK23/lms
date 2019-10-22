14ef7262bbce23907c7d8f725ed3806afd151505
TharusanK23 <ktharu0305@gmail.com>
2019-10-22 14:57:13 +0530

    lms

---
+++ js/main.js
@@ -0,0 +1,98 @@
+
+
+// /*===============================================Sidebar Start================================================*/
+
+
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+});
+
+
+
+// /*===============================================Sidebar End================================================*/
+
+
+
+
+// /*===============================================BACK TO TOP Start================================================*/
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+// /*===============================================BACK TO TOP End================================================*/
+
+
+
+
+// /*===============================================Sidebar Dropdown Start================================================*/
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
+/*===============================================Sidebar Dropdown End================================================*/
+
+
+
+// /*===============================================Form Start================================================*/
+
+
+// /*===============================================Form Start================================================*/
+
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeds")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
